c5041c290eba37731e2e5894cc535c91eb3f245b
olalekan <bhadmuslekan@gmail.com>
2022-09-08 12:31:21 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Print without using printf and put function
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
