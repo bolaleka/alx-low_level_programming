4f3f06b565e77db8d20fea5a1b704e1924998761
olalekan <bhadmuslekan@gmail.com>
2022-09-13 08:22:40 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+/**
+* _isalpha - Checks for alphabet character
+* @c: variable to checks
+*
+* Return: 0
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
