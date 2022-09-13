a73bc3806e5b691c0e44e52bdd305fa2189e6fe6
olalekan <bhadmuslekan@gmail.com>
2022-09-13 07:22:35 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "1-alphabet.c"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * 
+ * Return: Null
+ */
+void print_alphabet_x10(void)
+{
+	char count;
+
+	count = '0';
+	while (count <= '9')
+	{
+		print_alphabet();
+		count++;
+	}
+}
