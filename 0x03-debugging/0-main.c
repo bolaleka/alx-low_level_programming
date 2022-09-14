a46fa60a7c21702f2dc0d15ebd1f85629982969e
olalekan <bhadmuslekan@gmail.com>
2022-09-14 07:46:46 -0700

    goal

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,14 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
+ *
  * Return: 0
  */
-#ifdef MAIN
 int main(void)
 {
 	int i;
 
 	i = 98;
-	positive_or_negative();
+	positive_or_negative(i);
 	return (0);
 }
-#endif
