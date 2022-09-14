cf772080d7c3355e97f4d73ec95087983794f95a
olalekan <bhadmuslekan@gmail.com>
2022-09-13 18:44:03 -0700

    add number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - Add two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: total result wil be return
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
