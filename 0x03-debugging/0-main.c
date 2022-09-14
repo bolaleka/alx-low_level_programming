9d853c11d0a2e1ae869dd3b1ac78a782af0202d3
olalekan <bhadmuslekan@gmail.com>
2022-09-14 07:09:54 -0700

    goal

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,13 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-#ifdef LINUX
+#ifdef MAIN
 int main(void)
 {
 	int i;
 
 	i = 98;
-	positive_or_negative(i);
+	positive_or_negative();
 	return (0);
 }
 #endif
