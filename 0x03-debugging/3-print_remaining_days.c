b60b03943e9877c47f3d4fbb3259611adf02ff89
olalekan <bhadmuslekan@gmail.com>
2022-09-15 04:36:43 -0700

    Print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,33 +12,19 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
-		if ((month == 6 && day > 182) || (month == 11 && day > 307))
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		} else if (month > 12 || month < 1 || day < 1)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		} else
-		{
-			 printf("Day of the year: %d\n", day);
-			 printf("Remaining days: %d\n", 366 - day);
-		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day == 60 && !(year % 100 == 0))
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		} else if (month > 12 || month < 1 || day < 1)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		} else if ((month == 6 && day > 180) || (month == 11 && day > 305))
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
