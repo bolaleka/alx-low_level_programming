7644f5cc9f2ee30e25c737ed1b96715e2c063c65
olalekan <bhadmuslekan@gmail.com>
2022-09-15 13:39:46 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ * @n: input number od lines
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
