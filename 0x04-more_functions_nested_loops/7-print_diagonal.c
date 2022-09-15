5e9ba6a42c9a6d574b1ee44dec6e24b27902124e
olalekan <bhadmuslekan@gmail.com>
2022-09-15 14:02:55 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line on the teminal
+ * @n: input times of character
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			j = i;
+			while (j-- >= 0)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
