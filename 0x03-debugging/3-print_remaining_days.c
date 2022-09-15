c4bd80684afe86d738b9fab468e5636fc7157241
olalekan <bhadmuslekan@gmail.com>
2022-09-15 03:07:56 -0700

    Print valid date

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,10 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60 && !(year % 100 == 0))
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		} else if(month < 1 || day < 1)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
