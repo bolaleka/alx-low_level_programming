f0bf30e1cbe1826b57c9e948272faafa401d773b
olalekan <bhadmuslekan@gmail.com>
2022-09-19 09:48:21 -0700

    String reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "./2-strlen.c"
+
+/**
+ * print_rev - prints string in reverse followed by a new line
+ * @s: input string character
+ *
+ * Retturn: void
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	for (; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
