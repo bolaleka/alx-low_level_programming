bc5ce754eb99e3fff3f2621736692f5f5f7cb3af
olalekan <bhadmuslekan@gmail.com>
2022-09-19 17:22:35 -0700

    increment by 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,11 +13,8 @@ void puts2(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(str[i]);
+		i = i + 2;
 	}
 	_putchar('\n');
 }
