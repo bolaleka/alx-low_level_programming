2b577278f4056a5d3740bd07ac03e476d11f4112
olalekan <bhadmuslekan@gmail.com>
2022-09-19 18:07:38 -0700

    Puts half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "./2-strlen.c"
+/**
+ * puts_half = Prints half of a string
+ * @str: input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, len, halflen;
+
+	len = _strlen(str);
+	i = len / 2;
+	if (len % 2 != 0)
+	{
+		halflen = (len - 1) / 2;
+		while ( halflen < len)
+		{
+			_putchar(str[halflen]);
+			halflen++;
+		}
+	} else
+	{
+		while (i < len)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
