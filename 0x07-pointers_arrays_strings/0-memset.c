a07008651b21eef33fc705c18a9d686b64a5187d
olalekan <bhadmuslekan@gmail.com>
2022-09-26 04:51:49 -0700

    Set memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	if (*s == NULL)
-		return (NULL);
-	while (n--)
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
 	{
-		*s++ = b;
+		s[i] = b;
+		i++;
 	}
 	return (s);
 }
