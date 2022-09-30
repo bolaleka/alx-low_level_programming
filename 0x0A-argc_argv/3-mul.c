cff0c24c22d3f6aac00418e20b65c301d862b38a
olalekan <bhadmuslekan@gmail.com>
2022-09-30 04:13:43 -0700

    multiply argument value

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main -  Entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	mul = 0;
+	if (argc == 1)
+	{
+		printf("%s\n", "Error");
+		return (0);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (0);
+}
