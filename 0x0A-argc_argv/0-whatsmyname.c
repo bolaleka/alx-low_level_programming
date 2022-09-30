f682d1b063b70cf8be2d844014a5cb651e49888a
olalekan <bhadmuslekan@gmail.com>
2022-09-30 03:03:08 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  Entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
