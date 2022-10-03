691915e8daf7c250b5b65b8899f9713914975842
olalekan <bhadmuslekan@gmail.com>
2022-10-03 07:31:02 -0700

    malloc pointer

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - Creates an array of char, and initialiaze
+ * @size: input size
+ * @c: input character
+ *
+ * Return: pointer to array / NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	if (str == NULL)
+		return (NULL);
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0B. C - malloc, free #
+## Using malloc and free in C function ##
