6f6ca87bf298b3dec2ccb919c0b9abca64dc816d
olalekan <bhadmuslekan@gmail.com>
2022-10-03 09:48:05 -0700

    concatenate string by allocating memory

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "./2-strlen.c"
+
+/**
+ * str_concat - function concatenates two strings
+ * @s1: first param
+ * @s2: second param
+ *
+ * Return: string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i, j, len;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	len = _strlen(s1) + _strlen(s2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < _strlen(s2); j++)
+	{
+		str[i + j] = s2[j];
+	}
+	str[len] = '\0';
+	return (str);
+}
