9593a21bd20b5d2ecb2fc811a8b1925b41657380
olalekan <bhadmuslekan@gmail.com>
2022-10-03 08:16:26 -0700

    malloc pointer

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,14 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
-	
-	if (size == 0)
-		return (NULL);
+
 	str = malloc(sizeof(char) * size);
+	if (str == NULL || size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
 	}
 	str[size] = '\0';
-	if (str != NULL)
-		return (str);
-	return (NULL);
+	return (str);
 }
