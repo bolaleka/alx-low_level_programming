4141db357cd62e945edfc9460ae376702f765fbb
olalekan <bhadmuslekan@gmail.com>
2022-10-04 07:39:31 -0700

    Concatenate string and return allocated memory space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,30 +11,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j, len;
+	int i, j, t, p, len;
 
 	if (s1 != NULL && s2 == NULL)
 	{
-		return (s1);
-	} else if (s1 == NULL && s2 != NULL)
+		str = malloc(sizeof(char) * _strlen(s1) + 1);
+		for (t = 0;  t < _strlen(s1); t++)
+			str[t] = s1[t];
+		return (str);
+	}
+	if (s1 == NULL && s2 != NULL)
 	{
-		return (s2);
-	} else if (s1 == NULL && s2 == NULL)
+		str = malloc(sizeof(char) * _strlen(s2) + 1);
+		for (p = 0; p < _strlen(s2); p++)
+			str[p] = s2[p];
+		return (str);
+	}
+	if (s1 == NULL && s2 == NULL)
 	{
-		return (NULL);
+		str = malloc(sizeof(char) * 2);
+		str[0] = '\n';
+		return (str);
 	}
 	len = _strlen(s1) + _strlen(s2);
 	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < _strlen(s1); i++)
-	{
 		str[i] = s1[i];
-	}
 	for (j = 0; j < _strlen(s2); j++)
-	{
 		str[i + j] = s2[j];
-	}
-	str[len] = '\0';
 	return (str);
 }
