ba209359f9902dab18cd1a6525311dcaa98e2c06
olalekan <bhadmuslekan@gmail.com>
2022-10-05 03:16:20 -0700

    concat program argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "./2-str_concat.c"
+
+/**
+ * argstostr - Concatenates all the argument of a program
+ * @ac: argument count
+ * @av: argument value
+ *
+ * Return: string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+		len += _strlen(av[i]) + 1;
+	str = (char *)malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		str = str_concat(str, av[i]);
+		str = str_concat(str, "\n");
+	}
+	return (str);
+}
