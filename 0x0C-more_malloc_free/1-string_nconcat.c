05838bce6a37f312d5a3e87dccd9dc31335e82c6
olalekan <bhadmuslekan@gmail.com>
2022-10-06 11:06:49 -0700

    malloc manipulation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,32 +12,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j;
+	unsigned int i, j, t, p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	s = NULL;
 
-	if (n < _strlen(s2))
-		s = malloc(sizeof(char) * (_strlen(s1) + n) + 1);
-	else
-		s = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
-	if (s == NULL)
-		return (NULL);
-	for (i =  0; i < _strlen(s1); i++)
-		s[i] = s1[i];
-	if (n < _strlen(s2))
+	if (s1 != NULL && s2 == NULL)
 	{
-		for (j = 0; j < n; j++)
+		s = malloc(sizeof(char) * _strlen(s1) + 1);
+		for (t = 0; t < _strlen(s1); t++)
+			s[t] = s1[t];
+		return (s);
+	} else if (s1 == NULL && s2 != NULL)
+	{
+		s = malloc(sizeof(char) * _strlen(s2) + 1);
+		for (p = 0; p < _strlen(s2); p++)
+			s[p] = s2[p];
+		return (s);
+	} else if (s1 == NULL && s2 == NULL)
+	{
+		s = malloc(sizeof(char) * 1);
+		s[0] = '\0';
+		return (s);
+	} else if (n < _strlen(s2))
+	{
+		s = malloc(sizeof(char) * (_strlen(s1) + n) + 1);
+		for (i = 0; i < n && s1[i] != '\0'; i++)
+			s[i] = s1[i];
+		for (j = 0; j < n && s2[j] != '\0'; j++)
 			s[i + j] = s2[j];
-		s[_strlen(s2) + j] = '\0';
+		s[_strlen(s2) + i] = '\0';
 	} else
 	{
-		for (j = 0; j < _strlen(s2); j++)
+		s = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
+		for (i = 0; i < (_strlen(s1) + _strlen(s2)) && s1[i] != '\0'; i++)
+			s[i] = s1[i];
+		for (j = 0; j < (_strlen(s1) + _strlen(s2)); j++)
 			s[i + j] = s2[j];
-		s[_strlen(s2) + j] = '\0';
+		s[_strlen(s2) + i] = '\0';
 	}
 	return (s);
 }
