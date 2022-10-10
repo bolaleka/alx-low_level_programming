03ef5878133092399511e21e8734b19cb63b7cb6
olalekan <bhadmuslekan@gmail.com>
2022-10-10 05:59:27 -0700

    structure manipulation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - structure for dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Discription: structure for the type struct dog
+ */
 struct dog
 {
 	char *name;
