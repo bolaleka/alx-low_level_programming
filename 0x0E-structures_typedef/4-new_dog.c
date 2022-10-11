6b3d1840b5e7edd20f49bc788e7cdd7ce20d3153
olalekan <bhadmuslekan@gmail.com>
2022-10-10 18:52:00 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
 
-	my_dog = (dog_t *)malloc(sizeof(*my_dog));
+	my_dog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
-	my_dog->owner = owner;
+	my_dog->name = malloc(_strlen(name) + 1);
+	my_dog->owner = malloc(_strlen(owner) + 1);
+	_strcpy(my_dog->name, name);
+	_strcpy(my_dog->owner, owner);
 	my_dog->age = age;
-	return my_dog;
+	return (my_dog);
 }
---
+++ 0x0E-structures_typedef/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "./dog.h"
+#include "./2-strlen.c"
+/**
+ * _strcpy - copy string pointed to by src including null byte
+ * @dest: destination param
+ * @src: source param
+ *
+ * Return: destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	if (dest == NULL)
+		return (NULL);
+	len = _strlen(src);
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
