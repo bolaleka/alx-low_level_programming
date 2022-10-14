33989955a604900763d8c5ca94725ad1642b3601
olalekan <bhadmuslekan@gmail.com>
2022-10-13 20:25:53 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints stringi
+ * @separator: first param
+ * @n: paramete length
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list param;
+	unsigned int i;
+	char *res;
+
+	va_start(param, n);
+
+	if (n == 0 || separator == NULL)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		res = va_arg(param, char *);
+		printf("%s", res);
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(param);
+}
