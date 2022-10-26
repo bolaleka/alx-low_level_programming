b34d24706c94c26229411d69f21ced273fdcfae6
olalekan <bhadmuslekan@gmail.com>
2022-10-26 03:45:49 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "./2-strlen.c"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: the main string
+ * @needle: substring to search for
+ *
+ * Return: substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int count;
+
+	for (i = 0; i  < _strlen(haystack); i++)
+	{
+		count = 0;
+		for (j = 0; j < _strlen(needle); j++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				count++;
+				i++;
+			} else
+				break;
+		}
+		if (_strlen(needle) == count)
+			return (needle);
+	}
+	return (NULL);
+}
