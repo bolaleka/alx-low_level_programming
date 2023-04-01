ccdf2461f45e6128d088519be2d81f95721c09ca
olalekan <bhadmuslekan@gmail.com>
2023-04-01 09:14:56 -0700

    Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	i = 0;
-	if (size <= 0 || array == NULL || cmp(array[i] == 0))
+	if (size <= 0 || array == NULL || cmp == NULL )
 	{
 		return (-1);
 	}
