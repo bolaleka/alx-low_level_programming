e27046161199f68ef66044044e9d45c23cd822a0
olalekan <bhadmuslekan@gmail.com>
2023-04-01 09:05:53 -0700

    Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,22 +4,26 @@
  * int_index - Function searches for an intger
  * @array: array input
  * @size: number of element in array
- * @(*cmp)(int): pointer to the function
+ * @cmp: pointer to the function
  *
  *Return: integer
  */
-int int_index(int *array, int size, int (*cmp)(int)) {
+int int_index(int *array, int size, int (*cmp)(int))
+{
 	int i;
 
 	i = 0;
-	if (size <= 0) {
-		return -1;
+	if (size <= 0)
+	{
+		return (-1);
 	}
-	while (i < size) {
-		if (cmp(array[i]) != 0) {
-			return i;
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
 		}
 		i++;
 	}
-	return -1;
+	return (-1);
 }
