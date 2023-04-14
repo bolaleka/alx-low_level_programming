a14a05a4aa6e8a8c66bead62f9e73bdbf227ee37
olalekan <bhadmuslekan@gmail.com>
2023-04-14 01:25:30 -0700

    Search Algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include "stdio.h"
 
 /**
  * binary_search - Search for a value  in a sorted array
@@ -12,15 +11,26 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, low, high, mid;
+	size_t i, low, high, j, mid;
 
 	low = 0;
 	high = size - 1;
 	if (array == NULL || size == 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= high; i++)
 	{
 		mid = (low + high) / 2;
+
+		if (low != high)
+			printf("Searching in array: ");
+		else
+		{
+			printf("Searching in array: %ld\n", low);
+			break;
+		}
+		for (j = low; j <= high; j++)
+			printf("%d, ", array[j]);
+		printf("\n");
 		if (array[mid] == value)
 		{
 			return (mid);
